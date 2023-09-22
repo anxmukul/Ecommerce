cca3feb37d8d5131146945406bd0a378bfcfb5e9
anxmukul <kmukul1197@gmail.com>
2023-09-22 18:01:25 +0530

    Renaming products and images

---
+++ src/products.js
@@ -0,0 +1,115 @@
+import product1 from "./images/1.png";
+import product2 from "./images/2.png";
+import product3 from "./images/3.png";
+import product4 from "./images/4.png";
+import product5 from "./images/5.png";
+import product6 from "./images/6.webp";
+import product7 from "./images/7.webp";
+import product8 from "./images/8.webp";
+import sneaker1 from "./images/9.jpg";
+import sneaker2 from "./images/10.jpg";
+import sneaker3 from "./images/11.jpg";
+import sneaker4 from "./images/12.jpg";
+import sneaker5 from "./images/13.jpg";
+import sneaker6 from "./images/14.jpg";
+import sneaker7 from "./images/15.jpg";
+import sneaker8 from "./images/16.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Cannon M50 Camera",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "WLS Van Gogh Denim Jacket",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "LED Light Strips",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SPECTRUM LS TEE",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AUTO SERVICE SHIRT by GOLF WANG",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "DON'T TRIP UNSTRUCTURED HAT",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Nike Air Monarch IV",
+    price: 190,
+    productImage: sneaker1,
+  },
+  {
+    id: 10,
+    productName: "Unisex-Adult Super",
+    price: 320,
+    productImage: sneaker2,
+  },
+  {
+    id: 11,
+    productName: "Nike Waffle One Sneaker",
+    price: 225,
+    productImage: sneaker3,
+  },
+  {
+    id: 12,
+    productName: "Nike Running Shoe",
+    price: 165,
+    productImage: sneaker4,
+  },
+  {
+    id: 13,
+    productName: "Flat Slip On Pumps",
+    price: 180,
+    productImage: sneaker5,
+  },
+  {
+    id: 14,
+    productName: "Loafer Flats",
+    price: 150,
+    productImage: sneaker6,
+  },
+  {
+    id: 15,
+    productName: "Low-Top Trainers",
+    price: 300,
+    productImage: sneaker7,
+  },
+  {
+    id: 16,
+    productName: "Amore Fashion Stilettos",
+    price: 700,
+    productImage: sneaker8,
+  }
+];
