ce712ef1bcb278f2ff2088da70f4bea9cbecbb87
anxmukul <kmukul1197@gmail.com>
2023-09-22 18:03:26 +0530

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import "./App.css"
+import Shop from "./routes/shop/shop";
+import Cart from "./routes/cart/cart";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar/>
+      <Navbar/>
       <Routes>
-        <Route path="/"/>
-        <Route path="/cart"/>
+        <Route path="/" element={<Shop/>}/>
+        <Route path="/cart" element={<Cart/>}/>
       </Routes>
       </Router>
     </div>
