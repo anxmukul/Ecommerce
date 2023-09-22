f7dd6a8f96b4ca56eeeffc517c1f2f4f594ced6d
anxmukul <kmukul1197@gmail.com>
2023-09-23 01:00:22 +0530

    Creating context Api to get states in components where required

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import "./App.css"
+import "./App.css";
 import Shop from "./routes/shop/shop";
 import Cart from "./routes/cart/cart";
+import ShopContextProvider from "./context/shop-context";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Shop/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-      </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
