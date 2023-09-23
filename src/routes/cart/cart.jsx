7175165d55287a0ec6d99066c055d3873518d825
anxmukul <kmukul1197@gmail.com>
2023-09-23 15:07:14 +0530

    Creating cart component to display items added into cart

--- src/routes/cart/cart.jsx
+++ src/routes/cart/cart.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-
+import {React, useContext} from 'react'
+import { PRODUCTS } from '../../products'
+import { ShopContext } from "../../context/shop-context";
+import CartItem from './cart-item';
+import "./cart.css"
 function Cart() {
+  const { cartItems } = useContext(ShopContext);
   return (
     <div className="cart">   
-        <h1>Cart Item will be shown here</h1>
-      
+        <div>
+          <h1>Your Cart Items</h1>
+        </div>
+        <div className="cartItems">
+          {PRODUCTS.map((product)=>{
+            if(cartItems[product.id] !== 0){
+              return <CartItem data={product}/>
+            }
+          })}
+        </div>
     </div>
   )
 }
